ec7b0e74f8581aa9307f71e8a45ce26734ea8c5d
tec82 <ewerton.gomes@gmail.com>
2026-04-30 14:43:58 -0300

    colocando env o projeto

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # 🔥 carrega o .env
+
 from flask import Flask
 from models import db, User
 from flask_login import LoginManager
@@ -7,29 +12,38 @@ from routes.auth import auth_bp
 
 import cloudinary
 import cloudinary.uploader
-import cloudinary.api
 
+# ☁️ Config Cloudinary via ENV
 cloudinary.config(
-    cloud_name="dewdl5bvd",
-    api_key="658834264543314",
-    api_secret="9nKHfYhdxdGs7nFSrPDGExLt7rE"
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
 )
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
+
+# Flask
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# MySQL
+'''app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)'''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login' # Define para onde vai quem não está logado
+login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# REGISTRO DOS BLUEPRINTS
 app.register_blueprint(site_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(auth_bp)
